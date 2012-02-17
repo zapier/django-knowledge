14acb4934a45ffec3fa2a02eee2036e9ece8b074
Bryan Helmig <bryan@bryanhelmig.com>
2012-02-16 22:31:01 -0600

    split args for views

--- desk/views.py
+++ desk/views.py
@@ -4,23 +4,26 @@ from desk.models import Question, Response
 
 
 def desk_index(request,
-                template='django_desk/index.html'):
+               template='django_desk/index.html'):
     questions = Question.objects.all()
     return render(request, template, {'questions': questions})
 
 
 def desk_list(request,
-                tags=None, template='django_desk/list.html'):
+              tags=None,
+              template='django_desk/list.html'):
     questions = Question.objects.all()
     return render(request, template, {'questions': questions})
 
 
-def desk_thread(request, question_id,
-                slug=None, template='django_desk/thread.html'):
+def desk_thread(request,
+                question_id,
+                slug=None,
+                template='django_desk/thread.html'):
     question = get_object_or_404(Question, id=question_id)
     return render(request, template, {'question': question})
 
 
 def desk_ask(request,
-                template='django_desk/ask.html'):
+             template='django_desk/ask.html'):
     return render(request, template, {'form': 'form'})
