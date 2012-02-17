da644c1496022cd9054a96d7cd16db45bc18d07a
Bryan Helmig <bryan@bryanhelmig.com>
2012-02-16 22:28:50 -0600

    pep8 cleanup

--- desk/urls.py
+++ desk/urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('desk.views',
     url(r'^$', 'desk_index', name='desk_index'),
     url(r'^questions/$', 'desk_list', name='desk_list'),
     url(r'^questions/(?P<tags>[a-z-]+)/$', 'desk_list', name='desk_list_tags'),
-    url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-]+)/$', 'desk_thread', name='desk_thread'),
+    url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-]+)/$',
+        'desk_thread', name='desk_thread'),
     url(r'^ask/$', 'desk_ask', name='desk_ask'),
 )
